25bb3c189a18462b5f65e68a64e41a17b417bb87
Bui Minh Vu <vu.mihn.bui@topicus.nl>
2021-03-12 15:30:22 +0700

    Fix test case

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,7 +3,7 @@ import { App } from './app';
 
 describe('App', () => {
   it('test', () => {
-    const app = new App;
+    const app = new App();
     const result = app.run();
     expect(result).equal(true);
   });
